926fb19c28d4f3066b301e3e2d505ee028affc49
Jakub Olejnik <olejnjak@gmail.com>
2021-10-21 01:05:59 +0200

    🐛 Fix torino command name

--- Sources/TorinoCore/TorinoCore.swift
+++ Sources/TorinoCore/TorinoCore.swift
@@ -2,7 +2,13 @@ import ArgumentParser
 
 public struct TorinoCore: ParsableCommand {
     public static var configuration = CommandConfiguration(
-        subcommands: [Upload.self, Download.self, Bootstrap.self, Update.self]
+        commandName: "torino",
+        subcommands: [
+            Upload.self,
+            Download.self,
+            Bootstrap.self,
+            Update.self
+        ]
     )
     
     public init() {
