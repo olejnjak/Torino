bc34d037b146086ec22e4c6ba19760de4827e9bd
Jakub Olejnik <olejnjak@gmail.com>
2021-10-29 17:39:53 +0200

    🐛 Print errors to stderr

--- Sources/TorinoCore/Services/Logger.swift
+++ Sources/TorinoCore/Services/Logger.swift
@@ -14,22 +14,38 @@ struct Logger: Logging {
     }
     
     func logStdout(_ output: String...) {
-        print(type: "[OUTPUT]", output: output)
+        print(type: "[OUTPUT]", output: output, to: &StandardOutputStream.stdout)
     }
     
     func info(_ info: String...) {
-        print(type: "[INFO]", output: info)
+        print(type: "[INFO]", output: info, to: &StandardOutputStream.stdout)
     }
     
     func error(_ error: String...) {
-        print(type: "[ERROR]", output: error)
+        print(type: "[ERROR]", output: error, to: &StandardOutputStream.stderr)
     }
     
-    private func print(type: String, output: [String]) {
+    private func print<Stream: TextOutputStream>(type: String, output: [String], to stream: inout Stream) {
         Swift.print(
             type,
             output.map { $0.trimmingCharacters(in: .newlines).replacingOccurrences(of: "\n", with: "\n" + type) }
-                .joined(separator: " ")
+                .joined(separator: " "),
+            to: &stream
         )
+    }
+}
+
+private final class StandardOutputStream: TextOutputStream {
+    static var stdout = StandardOutputStream(stream: Darwin.stdout)
+    static var stderr = StandardOutputStream(stream: Darwin.stderr)
+    
+    private let stream: UnsafeMutablePointer<FILE>
+    
+    private init(stream: UnsafeMutablePointer<FILE>) {
+        self.stream = stream
+    }
+    
+    func write(_ string: String) {
+        fputs(string, stream)
     }
 }
