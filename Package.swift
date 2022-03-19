7094b218af3fb72abc05cb459e5533f34cf4b479
Jakub Olejnik <olejnjak@gmail.com>
2022-03-19 14:00:10 +0100

    🔧 Require macOS 12

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Torino",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v12)],
     products: [
         .executable(
             name: "torino",
